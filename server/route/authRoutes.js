738959cf77fa72ce2cb6abd932673b0e783f8837
Andrey Dolya <dolya7kk@gmail.com>
2023-11-23 14:30:33 +0200

    remove unused middleware

--- server/route/authRoutes.js
+++ server/route/authRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { handleSignIn, handleSignUp } from "../controller/authController.js";
-import { generateAccessToken } from "../middleware/auth/generateAccessToken.js";
-import { generateRefreshToken } from "../middleware/auth/generateRefreshToken.js";
 
 const router = express.Router();
 
 router.post("/signup", handleSignUp);
-router.post("/signin", handleSignIn, generateAccessToken, generateRefreshToken);
+router.post("/signin", handleSignIn);
 
 export default router;
