e0ae4d114dd765052a913c9d7ff759c540b13215
Andrey Dolya <dolya7kk@gmail.com>
2023-11-29 20:43:15 +0200

    display devices connection status

--- client/src/components/DeviceCard.jsx
+++ client/src/components/DeviceCard.jsx
@@ -1,12 +1,13 @@
 const DeviceCard = ({ device }) => {
   return (
     <div className="device-card">
+      <p className="connected">
+        {device.connected ? "Connected" : "Disconnected"}
+      </p>
       <p className="identifier">{device.deviceIdentifier}</p>
       <p className="host-name">{device.hostName}</p>
       <p className="user-info">{device.userInfo.username}</p>
-      <div>
-        {device.ipAddress}
-      </div>
+      <div>{device.ipAddress}</div>
     </div>
   );
 };
